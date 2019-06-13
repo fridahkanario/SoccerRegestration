f691a5b90e777fabc76471bc1d12529ba8487d1d
Laban Kim <labankimi@gmail.com>
2019-06-13 23:02:51 +0300

    Redesigned the pages to make them consistent

--- app/src/main/java/com/example/soccerregistration/PersonalInformation.java
+++ app/src/main/java/com/example/soccerregistration/PersonalInformation.java
@@ -4,16 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
-public class PersonalInformation extends AppCompatActivity{
+public class PersonalInformation extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     Button button_next2;
     Button button_previous;
+    String[] relationship = { "Self", "Brother", "Sister", "Mother" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_information);
+
+        //spinner country
+        Spinner relation =findViewById(R.id.relationship_player);
+        ArrayAdapter<String> adaptercountry = new ArrayAdapter<String>(PersonalInformation.this, android.R.layout.simple_spinner_item, relationship);
+        adaptercountry.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        relation.setAdapter(adaptercountry);
+        relation.setOnItemSelectedListener(this);
 
         button_next2=findViewById(R.id.btn_next2);
         button_previous=findViewById(R.id.btn_previous);
@@ -34,6 +45,16 @@ public class PersonalInformation extends AppCompatActivity{
                 startActivity(mainActivity);
             }
         });
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 
