cd09c34b7c2192c2acae68911b83aca5929b7667
Fridah Kanario <kanariofridah11@gmail.com>
2019-06-14 11:13:40 +0300

    Added mySingleton class to connect to backend

--- app/src/main/java/com/example/soccerregistration/MainActivity.java
+++ app/src/main/java/com/example/soccerregistration/MainActivity.java
@@ -3,27 +3,52 @@ package com.example.soccerregistration;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.*;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    public static final String url = "http://localhost:8080/register";
+    private static final String TAG = "MainActivity";
+
+
     String[] countries = { "Kenya", "Uganda", "Ethiopia", "Rwanda", "Tanzania" };
     String[] counties = { "Nairobi", "Kiambu", "Machakos", "Mombasa", "Nakuru" };
     Button button_next;
+    EditText club_name;
+    EditText player_name;
+    //RequestQueue requestQueue;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        club_name=findViewById(R.id.club_name);
+        player_name=findViewById(R.id.player_name);
+
 
         button_next=findViewById(R.id.btn_next);
 
         button_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                register();
                 Intent personal_info = new Intent(getApplicationContext(), PersonalInformation.class);
                 startActivity(personal_info);
             }
@@ -42,8 +67,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         adaptercounty.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         county.setAdapter(adaptercounty);
         county.setOnItemSelectedListener(this);
-    }
 
+    }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -52,6 +77,54 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+
+    //send data to the database
+    public void register(){
+
+        Log.d(TAG, "register");
+        club_name.getText().toString();
+        player_name.getText().toString();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(getBaseContext(), "Login success", Toast.LENGTH_LONG).show();
+                        //onLoginSuccess();
+                        // response
+                        Log.d("Response", String.valueOf(response));
+                    }
+                },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // error
+                                Toast.makeText(getBaseContext(), "Register failed", Toast.LENGTH_LONG).show();
+                                //onLoginFailed();
+                                Log.d("Error.Response", "error");
+                            }
+                        }
+                ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<>();
+                params.put("Content-Type", "application/json");
+                params.put("country", String.valueOf(countries));
+                params.put("county", String.valueOf(counties));
+                params.put("club_name", String.valueOf(club_name));
+                params.put("player_name", String.valueOf(player_name));
+
+                return params;
+            }
+        };
+        // MySingleton.getInstance(this).addToRequestQueue(postRequest);
+        Volley.newRequestQueue(this).add(jsonObjectRequest);
 
     }
 }
